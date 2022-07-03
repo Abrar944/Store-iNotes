0e7f01215643c2bd5583ea797574592c413a53a0
Mohammed Abrar Ahmed <ahmedabrar944@gmail.com>
2022-07-03 10:57:36 -0700

    NavbarBuild

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
-
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import About from "./Components/About";
+import Home from "./Components/Home";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
       <h1>iNotesbooks</h1>
+      <BrowserRouter>
+    <Routes>
+      <Route  path="Navbar" element={<Navbar />}></Route>
+      <Route  path="HomePage" element={<Home />}></Route>
+      <Route  path="AboutPage" element={<About />}></Route>
+    </Routes>
+  </BrowserRouter>
+      
     </div>
   );
 }
